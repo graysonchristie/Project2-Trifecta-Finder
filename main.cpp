5fe5712f3641d615ac6ac44dbd6b423bdc091da7
Bellatar <142323364+Bellatar@users.noreply.github.com>
2026-07-10 19:11:05 -0400

    Edited main to test hash and fix some bugs

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include <vector>
 #include <string>
 #include "SongLoader.h"
+#include "SongLoader.h"
+#include "HashTable.h"
+
 
 using namespace std;
 
-int main() {
+int main() {/*
     string filename =
         "C:/Users/gchri/CLionProjects/Project2-Trifecta-Finder/dataset.csv";
 
@@ -25,7 +28,48 @@ int main() {
         }
     }
 
-    cout << "Matches found shown: " << found << endl;
+    cout << "Matches found shown: " << found << endl; */
+    string filename = "/Users/kentarsitano/Desktop/COP3530/Project2-Trifecta-Finder3/dataset.csv";
+
+    // Load songs using SearchEntry
+    vector<SearchEntry> songs = SongLoader::loadSongs(filename);
+    cout << "Loaded " << songs.size() << " searchable entries." << endl;
+
+    // Init Hash
+    HashTable ht(200003);
+
+    // Populate Hash Table using the 'key' (either Artist or Song Title)
+    cout << "Populating Hash Table with prefixes from keys..." << endl;
+    for (const auto& entry : songs) {
+        // entry.key -> index the prefix
+        // entry.display -> the value
+        string key = entry.key;
+        for (size_t i = 1; i <= key.length(); ++i) {
+            string prefix = key.substr(0, i);
+            ht.insert(prefix, entry.display);
+        }
+    }
+    cout << "Hash Table populated successfully!" << endl;
+
+    // Testing Hash Search Functionality
+    // Swap to test both artist and song title
+    vector<string> testQueries = {"Olivia Rodrigo", "Creep"};
+
+    for (const string& query : testQueries) {
+        cout << "\nTesting search for: \"" << query << "\"" << endl;
+
+        vector<string> results = ht.search(query);
+
+        if (results.empty()) {
+            cout << "No matches found." << endl;
+        } else {
+            cout << "Found " << results.size() << " matches:" << endl;
+            // Print 5 results
+            for (int i = 0; i < results.size() && i < 5; ++i) {
+                cout << "  - " << results[i] << endl;
+            }
+        }
+    }
 
     return 0;
 }
