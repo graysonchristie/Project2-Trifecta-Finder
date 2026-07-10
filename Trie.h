64935126d63fec2ee89c4d038346d0894778ea2e
zacha <zacharybosworth110@gmail.com>
2026-07-10 18:33:57 -0400

    Merely added includes, and namespace std for completion later, as well as a struct

--- Trie.h
+++ Trie.h
@@ -5,4 +5,23 @@
 #ifndef PROJECT2_TRIFECTA_FINDER_TRIE_H
 #define PROJECT2_TRIFECTA_FINDER_TRIE_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+struct TrieNode {
+    TrieNode* children[26];
+    bool isEndOfWord;
+    TrieNode(){
+        isEndOfWord = false;
+        for (int i = 0; i < 26; i++) children[i] = nullptr;
+    }
+};
+
+
+
+
 #endif //PROJECT2_TRIFECTA_FINDER_TRIE_H
